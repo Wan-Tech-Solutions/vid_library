24696cdc45740c43195ad02e43e2b1a4c3850bca
Akatious1 <sule.aktious@gmail.com>
2025-07-13 13:54:21 +0000

    Database seeders

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::firstOrCreate(
+            ['email' => 'sule.aktious@gmail.com'],
+            [
+                'name' => 'Admin',
+                'password' => bcrypt('22Secured'),
+                'role' => 'superadmin',
+            ]
+        );
+    }
+}
